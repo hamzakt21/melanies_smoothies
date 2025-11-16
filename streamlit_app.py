21f797d0144d333b9778051a3d04c8dc9f577232
hamzakt21 <hamzakt69@gmail.com>
2025-11-16 08:39:54 +0530

    removed df streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,6 @@ name_on_order = st.text_input("Name on Smoothie")
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"), col("SEARCH_ON"))
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:', my_dataframe, max_selections=5
